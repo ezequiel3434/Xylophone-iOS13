d90c3cdf59e0e1ab6f66f7efa9076cba1bb0843c
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-19 12:43:23 -0300

    playing differents xylopone sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print()
+        playSound(sender.currentTitle!)
+    }
     
-
+    func playSound(_ title: String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
